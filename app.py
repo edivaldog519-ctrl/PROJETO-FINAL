a0aa5debaab69ad9ded5eabf417140100776ef90
Edivaldo <edivaldog519@gmail.com>
2025-10-14 21:48:30 -0300

    Início da aplicação

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from core.aluno.aluno_controller import aluno_controller
+
+
+app = Flask(__name__)
+
+# registro das controlles
+app.register_blueprint(aluno_controller)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
